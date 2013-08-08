e436481d230268e12d0ded28f73da3db278de98a
Ale Feltes <awi@alefq.com>
2013-08-08 17:34:46 -0400

    Aplicación base para un servidor y cliente de chat

---
+++ chat/src/main/java/py/edu/uca/edw/java2/chat/client/clientRunner.java
@@ -0,0 +1,12 @@
+package py.edu.uca.edw.java2.chat.client;
+
+class clientRunner 
+{
+//I sinartisi pou kaleite gia na ani3ei o client
+//dimiourgite antikeimeno kai trexi apo alli klasi logo provlimatwn me static prosvaseis klp
+	public static void main(String args[])
+	{
+		client c = new client();
+		c.run();
+	}    
+}
